4d64ee125431fcc9cf7a7e980e6eb1bf47fe25a5
Guilherme Araujo Thomaz <guilhermepic2016@gmail.com>
2021-01-24 13:02:58 -0300

    Transaction family client constants (macros) file added

---
+++ transaction_family/src/sawtooth_autavail_client/constants.go
@@ -0,0 +1,21 @@
+package main
+
+const (
+	// String literals
+	FAMILY_NAME       string = "autavail"
+	FAMILY_VERSION    string = "0.0"
+	DISTRIBUTION_NAME string = "sawtooth-autavail"
+	DEFAULT_URL       string = "http://sawtooth-rest-api-default-0:8008"
+	// Verbs
+	VERB_ADVERT string = "advert"
+	VERB_BUY string = "buy"
+	// APIs
+	BATCH_SUBMIT_API string = "batches"
+	BATCH_STATUS_API string = "batch_statuses"
+	STATE_API        string = "state"
+	// Content types
+	CONTENT_TYPE_OCTET_STREAM string = "application/octet-stream"
+	// Integer literals
+	FAMILY_NAMESPACE_ADDRESS_LENGTH uint = 6
+	FAMILY_VERB_ADDRESS_LENGTH      uint = 64
+)
