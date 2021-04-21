85cc8fe24515179d0f9c282a1b31bc377977d6b6
Guilherme Araujo Thomaz <guilhermepic2016@gmail.com>
2021-04-21 16:09:26 -0300

    Changes done

--- transaction_family/post-install.sh
+++ transaction_family/post-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run $1 cat /go/src/github.com/hyperledger/sawtooth-sdk-go/examples/autavail_go/bin/autavail-go > ./autavail-go
+cmd="docker run $1 cat /go/src/github.com/hyperledger/sawtooth-sdk-go/examples/autavail_go/bin/autavail-go"
+$cmd > ./autavail-go
 chmod +x ./autavail-go
 mkdir ../sawtooth_benchmark/binary/v3/
 mv ./autavail-go ../sawtooth_benchmark/binary/v3/
